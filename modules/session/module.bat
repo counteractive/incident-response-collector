4be7e9ce33130af83a0ecf3dac269b395ce93ea6
sgarelick <55923131+sgarelick@users.noreply.github.com>
2020-08-11 10:55:34 -0500

    Update module.bat

--- modules/session/module.bat
+++ modules/session/module.bat
@@ -1,22 +1,22 @@
-:: for sessions
-
 @echo off
 setlocal enableextensions enabledelayedexpansion
+
+:: name: sessioon
+:: version: 1.0.0
+:: author: Sarah Garelick
+:: description: collects data on computer sessions and the session user
+:: notes: this module is licensed per the terms in the LICENSE file at the root of this project. 3rd-party tools are used and/or shared per the terms of their respective licences. See the NOTICE file at the root of this project for details.
+:: tags: processed
 
 set _mod_name=sessions
 
 :setup
-  :: TODO: remove when implemented
-  set _mod_util=..\..\util
-
-  ::call "%_mod_util%\log" "[%_mod_name%] module not implented" "%_LOG%"
-  ::popd & exit /B 0
 
   :: operate within this module's directory
   pushd "%~dp0"
   if "%_DBG%" == "true" echo DEBUG: [%_mod_name%] working in %cd%
 
-  ::set _mod_util=..\..\util
+  set _mod_util=..\..\util
   set _mod_tools=tools
 
   if not exist "%_mod_util%" (
@@ -34,11 +34,11 @@ set _mod_name=sessions
   )
 
   :: confirm this module is enabled
-  ::call "%_mod_util%\read-config" %_mod_name% _mod_enabled
-  ::if not "%_mod_enabled%" == "true" (
-  ::  call "%_mod_util%\log" "[%_mod_name%] module not enabled" "%_LOG%"
-  ::  popd & exit /B 0
-  ::)
+  call "%_mod_util%\read-config" %_mod_name% _mod_enabled
+  if not "%_mod_enabled%" == "true" (
+    call "%_mod_util%\log" "[%_mod_name%] module not enabled" "%_LOG%"
+    popd & exit /B 0
+  )
 
   call "%_mod_util%\log" "[%_mod_name%] started module setup" "%_LOG%"
   mkdir "%_mod_output%"
@@ -56,10 +56,6 @@ set _mod_name=sessions
     set _psloggedon=%_mod_tools%\PsLoggedon.exe
   )
 
-  ::set _querysession=qwinsta
-  ::set _queryuser=quser
-  ::echo "%_querysession%"
-
   call:module
   goto:eof
 
@@ -67,24 +63,24 @@ set _mod_name=sessions
   :: TODO: list TODO items
   call "%_mod_util%\log" "[%_mod_name%] started module" "%_LOG%"
 
+  echo "%_mod_tools%\PsLoggedon64.exe"
   echo "%_logonsessions%"
   call "%_mod_util%\log" "[%_mod_name%] collecting all active sessions" "%_LOG%"
-  call"%_mod_util%\exec" "%_logonsessions% -c -p" "%_mod_output%\logonsessions.log" "%_mod_name%"
+  call "%_mod_util%\exec" "%_logonsessions% -c -p" "%_mod_output%\logonsessions.log" "%_mod_name%"
   
   echo "%_psloggedon%"
   call "%_mod_util%\log" "[%_mod_name%] collecting information about user logged on" "%_LOG%"
-  call"%_mod_util%\exec" "%_psloggedon% -accepteula" "%_mod_output%\psloggedon.log" "%_mod_name%"
+  call "%_mod_util%\exec" "%_psloggedon% -accepteula" "%_mod_output%\psloggedon.log" "%_mod_name%"
 
   ::query session shows all active sessions (need special permission for more)
+  
+  call "%_mod_util%\log" "[%_mod_name%] querying all active sessions" "%_LOG%"
+  qwinsta >"%_mod_output%\querysessions.txt"
 
-  ::echo "qwinsta/counter"
-  ::call "%_mod_util%\log" "[%_mod_name%] querying all active sessions" "%_LOG%"
-  ::call"%_mod_util%\exec" "qwinsta/counter" "%_mod_output%\querysessions.log" "%_mod_name%"
   :: query user displays information about the user logged on
   :: specifically the name of user, name of session, session id, state of session, idle time, date and time logged on
-
-  ::call "%_mod_util%\log" "[%_mod_name%] querying all user sessions on server" "%_LOG%"
-  ::call"%_mod_util%\exec" "quser" "%_mod_output%\queryuser.log" "%_mod_name%"
+  call "%_mod_util%\log" "[%_mod_name%] querying all user sessions on server" "%_LOG%"
+  quser > "%_mod_output%\queryuser.txt"
 
   call "%_mod_util%\log" "[%_mod_name%] completed module" "%_LOG%" 
 
